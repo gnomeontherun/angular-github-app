fdcf5a58ebae6e0b120e6c992f23dd16d2e8c701
Jeremy Wilken <gnomation@gnomeontherun.com>
2015-04-17 21:23:39 -0500

    update angular material

--- app/config.js
+++ app/config.js
@@ -18,7 +18,7 @@ System.config({
     "angular": "github:angular/bower-angular@1.3.15",
     "angular-animate": "github:angular/bower-angular-animate@1.3.15",
     "angular-aria": "github:angular/bower-angular-aria@1.3.15",
-    "angular-material": "github:angular/bower-material@0.8.3",
+    "angular-material": "github:angular/bower-material@0.9.0-rc1",
     "babel": "npm:babel-core@5.1.10",
     "babel-runtime": "npm:babel-runtime@5.1.10",
     "core-js": "npm:core-js@0.8.4",
--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,14 @@
 import angular from 'angular'
+import material from 'angular-material'
+
+import 'angular-material/angular-material.css!'
 
 // Bootstrap the application when the application assets are ready.
 angular
   .element(document)
   .ready(function() {
 
-    let app = angular.module('githubApp', []);
+    let app = angular.module('githubApp', [material]);
 
     angular.bootstrap(document.body, [app.name]);
 
